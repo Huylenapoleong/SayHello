2c5d1181bdbdb5cb50937e3f854af6ef28549dae
huy <huylee2044@gmail.com>
2025-06-12 22:35:20 +0700

    Initial commit...

---
+++ src/hello/SayHello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class SayHello {
+	public String name = "LeQuangHuy";
+	public static void main(String[] arg)
+	{
+		SayHello ob1 = new SayHello();
+		System.out.print(ob1.name);
+		
+	}
+
+}
